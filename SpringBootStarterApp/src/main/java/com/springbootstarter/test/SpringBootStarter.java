ff35cd7552c33365c1872dcf2e486d37240403fc
Shantanu Mitra <Shantanu Mitra@Shantanu-PC>
2019-08-27 18:05:30 +0530

    committing

--- SpringBootStarterApp/src/main/java/com/springbootstarter/test/SpringBootStarter.java
+++ SpringBootStarterApp/src/main/java/com/springbootstarter/test/SpringBootStarter.java
@@ -2,14 +2,13 @@ package com.springbootstarter.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 
 public class SpringBootStarter {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	
 SpringApplication.run(SpringBootStarter.class, args);
 	}
 
